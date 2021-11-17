fa1c027face5247d3d17b4bb7822cc9c6d96f18f
Averyyyyyyyy <92644634+Averyyyyyyyy@users.noreply.github.com>
2021-11-17 19:51:23 -0400

    Add project files.

---
+++ SlayerCalculator/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿
+namespace SlayerCalculator
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXpNeeded = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.txtPricePerSlayer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtXpPerSlayer = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSlayerLeft = new System.Windows.Forms.TextBox();
+            this.lblLinkCredit = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(246, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 144);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tier";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(7, 116);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(52, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Tier 5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 92);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(52, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Tier 4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Tier 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tier 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tier 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Slayer XP Buff (+25%)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(163, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "SLASHED Pricing (half price)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Location = new System.Drawing.Point(246, 177);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 72);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Perks";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "+1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonAdd1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "-1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonSub1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 10;
+            // 
+            // txtXpNeeded
+            // 
+            this.txtXpNeeded.Location = new System.Drawing.Point(102, 25);
+            this.txtXpNeeded.Name = "txtXpNeeded";
+            this.txtXpNeeded.Size = new System.Drawing.Size(100, 20);
+            this.txtXpNeeded.TabIndex = 11;
+            this.txtXpNeeded.TextChanged += new System.EventHandler(this.TextChangedXpNeeded);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "XP Needed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Total Cost";
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTotalCost.Location = new System.Drawing.Point(102, 114);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.ReadOnly = true;
+            this.txtTotalCost.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalCost.TabIndex = 14;
+            this.txtTotalCost.Text = "N/A";
+            // 
+            // txtPricePerSlayer
+            // 
+            this.txtPricePerSlayer.Location = new System.Drawing.Point(102, 83);
+            this.txtPricePerSlayer.Name = "txtPricePerSlayer";
+            this.txtPricePerSlayer.ReadOnly = true;
+            this.txtPricePerSlayer.Size = new System.Drawing.Size(100, 20);
+            this.txtPricePerSlayer.TabIndex = 15;
+            this.txtPricePerSlayer.Text = "N/A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Price per Slayer";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "EXP per Slayer";
+            // 
+            // txtXpPerSlayer
+            // 
+            this.txtXpPerSlayer.Location = new System.Drawing.Point(102, 56);
+            this.txtXpPerSlayer.Name = "txtXpPerSlayer";
+            this.txtXpPerSlayer.ReadOnly = true;
+            this.txtXpPerSlayer.Size = new System.Drawing.Size(100, 20);
+            this.txtXpPerSlayer.TabIndex = 18;
+            this.txtXpPerSlayer.Text = "N/A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Slayers Left";
+            // 
+            // txtSlayerLeft
+            // 
+            this.txtSlayerLeft.BackColor = System.Drawing.SystemColors.Control;
+            this.txtSlayerLeft.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtSlayerLeft.Location = new System.Drawing.Point(102, 182);
+            this.txtSlayerLeft.Name = "txtSlayerLeft";
+            this.txtSlayerLeft.ReadOnly = true;
+            this.txtSlayerLeft.Size = new System.Drawing.Size(100, 20);
+            this.txtSlayerLeft.TabIndex = 21;
+            this.txtSlayerLeft.Text = "N/A";
+            // 
+            // lblLinkCredit
+            // 
+            this.lblLinkCredit.AutoSize = true;
+            this.lblLinkCredit.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.lblLinkCredit.Location = new System.Drawing.Point(328, 264);
+            this.lblLinkCredit.Name = "lblLinkCredit";
+            this.lblLinkCredit.Size = new System.Drawing.Size(118, 13);
+            this.lblLinkCredit.TabIndex = 22;
+            this.lblLinkCredit.TabStop = true;
+            this.lblLinkCredit.Text = "Made by: Avery with ♥";
+            this.lblLinkCredit.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblLinkCredit_LinkClicked);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 286);
+            this.Controls.Add(this.lblLinkCredit);
+            this.Controls.Add(this.txtSlayerLeft);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtXpPerSlayer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPricePerSlayer);
+            this.Controls.Add(this.txtTotalCost);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtXpNeeded);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(500, 325);
+            this.MinimumSize = new System.Drawing.Size(500, 325);
+            this.Name = "Form";
+            this.Text = "Slayer Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXpNeeded;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TextBox txtPricePerSlayer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtXpPerSlayer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSlayerLeft;
+        private System.Windows.Forms.LinkLabel lblLinkCredit;
+    }
+}
+
---
+++ SlayerCalculator/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlayerCalculator
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        /*
+        * Tier Cost and EXP
+        * 5: 100,000 Coins | 1,500 XP 
+        * 4: 50,000 Coins | 500 XP
+        * 3: 20,000 Coins | 100 XP
+        * 2: 7,500 Coins | 25 XP
+        * 1: 2,000 Coins | 5 XP
+        */
+
+
+        // variables
+        int tier = 0;
+        bool isXpPerk = false;
+        bool isPricePerk = false;
+        decimal pricePerSlayer = 0m;
+        decimal xpPerSlayer = 0;
+        decimal slayersLeft = 0;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            tier = 1;
+            main();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            tier = 2;
+            main();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            tier = 3;
+            main();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            tier = 4;
+            main();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            tier = 5;
+            main();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            isXpPerk = checkBox1.Checked;
+            main();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            isPricePerk = checkBox2.Checked;
+            main();
+        }
+
+        private void main()
+        {
+            decimal price = 0m;
+            decimal xp = 0;
+            switch (tier)
+            {
+                case 1:
+                    price = 2000m;
+                    xp = 5;
+                    pricePerSlayer = isPricePerk ? (price / 2) : price;
+                    xpPerSlayer = isXpPerk ? Decimal.Ceiling(xp * 1.25m) : xp;
+                    break;
+                case 2:
+                    price = 7500m;
+                    xp = 25;
+                    pricePerSlayer = isPricePerk ? (price / 2) : price;
+                    xpPerSlayer = isXpPerk ? Decimal.Ceiling(xp * 1.25m) : xp;
+                    break;
+                case 3:
+                    price = 20000m;
+                    xp = 100;
+                    
+                    pricePerSlayer = isPricePerk ? (price / 2) : price;
+                    xpPerSlayer = isXpPerk ? Decimal.Ceiling(xp * 1.25m) : xp;
+                    break;
+                case 4:
+                    price = 50000m;
+                    xp = 500;
+                    pricePerSlayer = isPricePerk ? (price / 2) : price;
+                    xpPerSlayer = isXpPerk ? Decimal.Ceiling(xp * 1.25m) : xp;
+                    break;
+                case 5:
+                    price = 100000m;
+                    xp = 1500;
+                    pricePerSlayer = isPricePerk ? (price / 2) : price;
+                    xpPerSlayer = isXpPerk ? Decimal.Ceiling(xp * 1.25m) : xp;
+                    break;
+                default:
+                    break;
+            }
+            decimal xpNeeded = 0;
+            Decimal.TryParse(txtXpNeeded.Text, out xpNeeded);
+            if (xpNeeded < 0)
+            {
+                txtXpNeeded.Text = "0";
+            }
+
+            // slayer left calculation
+            try
+            {
+                decimal input = Convert.ToDecimal(txtXpNeeded.Text);
+                slayersLeft = input / xpPerSlayer;
+                txtSlayerLeft.Text = Decimal.Ceiling(slayersLeft).ToString();
+                txtSlayerLeft.ForeColor = Color.Black;
+            }
+            catch
+            {
+                txtSlayerLeft.ForeColor = Color.Red;
+                txtSlayerLeft.Text = "Invalid Input";
+                slayersLeft = 0;
+            }
+
+            // total cost calculation
+            try
+            {
+                txtTotalCost.ForeColor = Color.Black;
+                txtTotalCost.Text = (slayersLeft * pricePerSlayer).ToString("C0");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), ex.Message);
+                txtTotalCost.ForeColor = Color.Red;
+                txtTotalCost.Text = "Invalid Input";
+                
+            }
+            txtPricePerSlayer.Text = pricePerSlayer.ToString("C0");
+            txtXpPerSlayer.Text = xpPerSlayer.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextChangedXpNeeded(object sender, EventArgs e)
+        {
+            main();
+        }
+
+        private void buttonAdd1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtXpNeeded.Text = (Convert.ToDecimal(txtXpNeeded.Text) - xpPerSlayer).ToString();
+                main();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), ex.GetType().ToString());
+            }
+        }
+
+        private void buttonSub1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtXpNeeded.Text = (Convert.ToDecimal(txtXpNeeded.Text) + xpPerSlayer).ToString();
+                main();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), ex.GetType().ToString());
+            }
+        }
+
+        private void lblLinkCredit_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Averyyyyyyyy");
+        }
+    }
+}
